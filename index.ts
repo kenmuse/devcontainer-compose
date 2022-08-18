3816e4b690c1f26ab798ee9699dfb2e4d449c858
Ken Muse <kenmuse@users.noreply.github.com>
2022-08-18 05:21:23 +0000

    Dockerfile-based version

--- index.ts
+++ index.ts
@@ -1,21 +1,10 @@
 import express, { Request, Response } from 'express';
-import * as redis from 'redis';
 
 const app = express();
 const port = process.env.PORT || 80;
-const client = redis.createClient({
-  socket: {
-   host: 'redis-stack',
-   port: 6379
-  }
-});
-
-client.connect();
-client.set('views', 0);
 
 app.get('/', async (request: Request, response: Response) => {
-    const views = await client.incr('views');
-    response.send(`Express App views: ${views}`);
+    response.send("Hello world!");
 });
 
 
